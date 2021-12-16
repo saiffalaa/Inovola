1321aa47cf61411771de146fe7903acedb5c2379
Saif-Alaa-ElDin <saifalaa95@gmail.com>
2021-12-16 02:37:55 +0200

    fixing spelling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="text-center section_space">
       <h1 className="font-primary">
-        طور متجرك و وسع أفاق تجارتك مع سوق نطبيقات زد
+        طور متجرك و وسع أفاق تجارتك مع سوق تطبيقات زد
       </h1>
       <p className="mt-5 w-50 mx-auto fs-4 fw-bold">
         طور تجارتك من خلال تطبيقات و حلول سوق تطبيقات زد التي تساعدك في نمو
